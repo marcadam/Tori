7bcf66a90561522fc5362a907131483a7c12137c
Marc A. Anderson <marc@marcadam.com>
2016-02-28 14:00:57 -0800

    Add account class

---
+++ Twitter/Account.swift
@@ -0,0 +1,25 @@
+//
+//  Account.swift
+//  Twitter
+//
+//  Created by Marc Anderson on 2/28/16.
+//  Copyright © 2016 Marc Anderson. All rights reserved.
+//
+
+import Foundation
+
+class Account {
+    static var accounts = [Account]()
+
+    var accessToken: String?
+    var user: User?
+
+    init(accessToken: String, user: User) {
+        self.accessToken = accessToken
+        self.user = user
+    }
+
+    class func addAccount(accessToken: String, user: User) {
+        Account.accounts.append(Account(accessToken: accessToken, user: user))
+    }
+}
